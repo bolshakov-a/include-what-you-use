588fb9e5f1a7071a151c2529a4e8303cb4590f2d
Bolshakov <bolsh.andrey@yandex.ru>
2022-07-13 12:31:13 +0300

    Test template specialization reporting

--- tests/cxx/member_expr.cc
+++ tests/cxx/member_expr.cc
@@ -58,6 +58,65 @@ void ViaMacro(const IndirectClass& ic) {
 }
 
 
+// Test that referencing template specialization members needs full type info
+// for all other members.
+
+// IWYU: IndirectTemplate needs a declaration
+// IWYU: IndirectClass needs a declaration
+int RefFn(const IndirectTemplate<IndirectClass>& ic) {
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  ic.Method();
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  return ic.a;
+}
+
+// IWYU: IndirectTemplate needs a declaration
+// IWYU: IndirectClass needs a declaration
+int PtrFn(const IndirectTemplate<IndirectClass>* ic) {
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  ic->Method();
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  return ic->a;
+}
+
+void TemplateStaticFn() {
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass needs a declaration
+  // IWYU: IndirectClass is...*indirect.h
+  IndirectTemplate<IndirectClass>::StaticMethod();
+}
+
+// IWYU: IndirectTemplate needs a declaration
+// IWYU: IndirectClass needs a declaration
+void ViaMacro(const IndirectTemplate<IndirectClass>& ic) {
+  // We should figure out we need IndirectTemplate and IndirectClass
+  // because of the 'ic.', even if the member-expr itself is in another file
+  // due to the macro.
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  ic.CALL_METHOD;
+
+  // Likewise, we 'own' this member expr because we own the dot.
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  IC.Method();
+  // IWYU: IndirectTemplate is...*indirect.h
+  // IWYU: IndirectClass is...*indirect.h
+  IC.CALL_METHOD;
+
+  IC.
+      // IWYU: IndirectTemplate is...*indirect.h
+      // IWYU: IndirectClass is...*indirect.h
+      CALL_METHOD;
+
+  // But this member-expr is entirely in the macro, so we don't own it.
+  IC_CALL_METHOD;
+}
+
 /**** IWYU_SUMMARY
 
 tests/cxx/member_expr.cc should add these lines:
@@ -67,7 +126,7 @@ tests/cxx/member_expr.cc should remove these lines:
 - #include "tests/cxx/direct.h"  // lines XX-XX
 
 The full include-list for tests/cxx/member_expr.cc:
-#include "tests/cxx/indirect.h"  // for IndirectClass
+#include "tests/cxx/indirect.h"  // for IndirectClass, IndirectTemplate
 #include "tests/cxx/member_expr-d1.h"  // for CALL_METHOD, IC, IC_CALL_METHOD
 
 ***** IWYU_SUMMARY */
